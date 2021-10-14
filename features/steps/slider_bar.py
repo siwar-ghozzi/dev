9be05e81ebb1dbd2205be9a742f4aa1488908866
Siwar Ghozzi <ghozzisiwar90@gmail.com>
2021-10-14 23:15:46 +0100

    minor

--- features/steps/slider_bar.py
+++ features/steps/slider_bar.py
@@ -1,10 +1,11 @@
 from behave import when, then
 from numpy.testing import assert_equal
+from allure import severity, severity_level
 import time
 
 from selenium.webdriver import ActionChains
 
-
+@severity(severity_level.MINOR)
 @when(u'he drag and drop the slider bar')
 def step_impl(context):
     context.dd.slider()
